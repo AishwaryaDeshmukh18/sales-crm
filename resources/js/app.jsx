ae3663969fb439f7f0723d53ee1dd1b1fe31d4f7
zwmhone <zunwuttmhone@gmail.com>
2026-02-24 02:47:08 +0630

    Resolve merge conflicts

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,13 +1,68 @@
+import "../css/app.css";
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import AppShell from "./Components/Layout/AppShell";
 
-function Dashboard() {
-    return <h1 style={{ margin: 0 }}>Dashboard</h1>;
+import ContactsListPage from "./pages/ContactsListPage";
+import ContactDetailPage from "./pages/ContactDetailPage";
+import CompanyListPage from "./pages/CompanyListPage";
+import CompanyDetailPage from "./pages/CompanyDetailPage";
+
+function HomePage() {
+    return <div>Home</div>;
 }
 
-createRoot(document.getElementById("app")).render(
-    <AppShell>
-        <Dashboard />
-    </AppShell>,
-);
+function Placeholder({ title }) {
+    return <div>{title}</div>;
+}
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<AppShell />}>
+                    <Route path="/" element={<HomePage />} />
+
+                    <Route path="/contacts" element={<ContactsListPage />} />
+                    <Route
+                        path="/contacts/:id"
+                        element={<ContactDetailPage />}
+                    />
+
+                    <Route path="/companies" element={<CompanyListPage />} />
+                    <Route
+                        path="/companies/:id"
+                        element={<CompanyDetailPage />}
+                    />
+
+                    <Route
+                        path="/opportunities"
+                        element={<Placeholder title="Opportunities" />}
+                    />
+                    <Route
+                        path="/stages"
+                        element={<Placeholder title="Stages" />}
+                    />
+                    <Route
+                        path="/pipeline"
+                        element={<Placeholder title="Pipeline" />}
+                    />
+                    <Route
+                        path="/csv-import"
+                        element={<Placeholder title="CSV Import" />}
+                    />
+                    <Route
+                        path="/search"
+                        element={<Placeholder title="Global Search" />}
+                    />
+
+                    <Route path="*" element={<Navigate to="/" replace />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+createRoot(document.getElementById("app")).render(<App />);
--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CsvImportController;
+use App\Http\Controllers\ContactsController;
+use App\Http\Controllers\CompaniesController;
 
 Route::post('/csv-import', [CsvImportController::class, 'store']);
+
+Route::get('/contacts', [ContactsController::class, 'index']);
+Route::get('/contacts/{id}', [ContactsController::class, 'show']);
+Route::post('/contacts/{id}/action', [ContactsController::class, 'applyAction']);
+Route::patch('/contacts/{id}', [ContactsController::class, 'updateContact']);
+
+Route::get('/companies', [CompaniesController::class, 'index']);
+Route::get('/companies/{id}', [CompaniesController::class, 'show']);
+Route::patch('/companies/{id}', [CompaniesController::class, 'updateCompany']);
+Route::post('/companies/{id}/action', [CompaniesController::class, 'applyAction']);
+Route::get('/companies/{id}/related-contacts', [CompaniesController::class, 'relatedContacts']);
+Route::get('/companies/{id}/related-deals', [CompaniesController::class, 'relatedDeals']);
